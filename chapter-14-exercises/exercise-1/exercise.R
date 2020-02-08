0ef008f53925dc865ff81d7a98778d23699977b8
Cheng-Hao Chen <johnson90435@KyleChen.local>
2020-02-07 20:26:49 -0800

    exercise week 5 complete

--- chapter-14-exercises/exercise-1/exercise.R
+++ chapter-14-exercises/exercise-1/exercise.R
@@ -2,46 +2,47 @@
 
 # Load the httr and jsonlite libraries for accessing data
 # You can also load `dplyr` if you wish to use it
-
-
+library(httr)
+library(jsonlite)
 # Create a variable base_uri that stores the base URI (as a string) for the 
 # Github API (https://api.github.com)
-
+base_uri <- "https://api.github.com"
 
 # Under the "Repositories" category of the API documentation, find the endpoint 
 # that will list _repos in an organization_. Then create a variable named
 # `org_resource` that stores the endpoint for the `programming-for-data-science`
 # organization repos (this is the _path_ to the resource of interest).
-
+org_resource <- "/orgs/programming-for-data-science/repos"
 
 # Send a GET request to this endpoint (the `base_uri` followed by the 
 # `org_resource` path). Print the response to show that your request worked. 
 # (The listed URI will also allow you to inspect the JSON in the browser easily).
-
+response <- GET(paste0(base_uri, org_resource))
 
 # Extract the content of the response using the `content()` function, saving it
 # in a variable.
-
+body <- content(response, "text")
 
 # Convert the content variable from a JSON string into a data frame.
-
-
+data <- fromJSON(body)
+print(data)
 # How many (public) repositories does the organization have?
+  #20
 
-
+View(data)
 # Now a second query:
 # Create a variable `search_endpoint` that stores the endpoint used to search 
 # for repositories. (Hint: look for a "Search" endpoint in the documentation).
-
+search_endpoint <- "/search/repositories?q={query}{&page,per_page,sort,order}"
 
 # Search queries require a query parameter (for what to search for). Create a 
 # `query_params` list variable that specifies an appropriate key and value for 
 # the search term (you can search for anything you want!)
-
+query_params <- list(q = "Lakers")
 
 # Send a GET request to the `search_endpoint`--including your params list as the
 # `query`. Print the response to show that your request worked.
-
+GET()
 
 # Extract the content of the response and convert it from a JSON string into a
 # data frame. 
